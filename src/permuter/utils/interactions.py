4500dc2dc6c75082af5e4a482d754f97cd29e83d
Carlos Grillet <carlosbeta5000@gmail.com>
2024-11-17 04:09:10 +0100

    refactor(utils): new modules added

---
+++ src/permuter/utils/interactions.py
@@ -0,0 +1,6 @@
+from utils.colors import Bgc
+
+ask_for_special_chars = Bgc.Yellow + "[?]Add special chars[" + \
+                        Bgc.End + " - _ . ! * " + Bgc.Yellow + "][y/n]: " + Bgc.End
+
+ask_for_minimum_password_length = Bgc.Yellow + "[?]Set the minimum password lenght [int]: " + Bgc.End
